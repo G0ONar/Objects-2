9b418077df3a7d271b117feafa2cc64b9dd2bbf9
G0ONar <142931976+G0ONar@users.noreply.github.com>
2025-10-30 16:08:56 -0700

    Create PetOrganizer.java

---
+++ PetOrganizer.java
@@ -0,0 +1,28 @@
+public class PetOrganizer {
+public class organizer{
+    public static void main(String args[]){
+        ShowDog showDog = new ShowDog("Buddy", 2, 7);
+        SportDog sportDog1 = new SportDog("Mittens", 5, 8);
+        SportDog sportDog2 = new SportDog("Nibbles", 3, 10);
+
+        Collections.sort(Showdog)
+        Collections.sort(Sportdog)
+
+        Iterator interator=ShowDog.interator();
+        while(iterator.hasNext()){  
+			Student student=(Student)iterator.next();  
+			System.out.println("Name: " + student.getName()+
+				  ", " + "RollNo: "+student.getRollNo()+
+					", Age: "+student.getAge()); 
+
+        Iterator interator=ShowDog.interator();
+        while(iterator.hasNext()){  
+			Student student=(Student)iterator.next();  
+			System.out.println("Name: " + student.getName()+
+				  ", " + "RollNo: "+student.getRollNo()+
+					", Age: "+student.getAge()); 
+}
+        }
+    }
+}
+}
